f2427154b244d5a6ce0b45e18114808f5abed3d9
Muhammad Haseeb <208623918+FalconCode786@users.noreply.github.com>
2025-10-16 10:41:45 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(180, 44);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(235, 28);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(180, 114);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(235, 28);
+            richTextBox2.TabIndex = 1;
+            richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(171, 85);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Validate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Enter Regular Expression";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Valid Regular Expression";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = richTextBox1.Text.Trim();
+            string[] words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Regex floatRegex = new Regex(@"\b[tT]\w*\b|\b[mM]\w*\b");
+            richTextBox2.Clear();
+            foreach (string word in words)
+            {
+                Match match = floatRegex.Match(word);
+                if (match.Success)
+                {
+                    richTextBox2.AppendText(word + " → Valid Float" + Environment.NewLine);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid (not a valid float or too long): " + word);
+                }
+
+            }
+        }
+    }
+}
